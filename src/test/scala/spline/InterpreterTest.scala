36114d02118128cf4ec8c3661dc0bc0d5e3d8211
d01c2 <rickykhj@korea.ac.kr>
2025-10-11 02:24:28 +0900

    feat: added unit tests for interpreter

---
+++ src/test/scala/spline/InterpreterTest.scala
@@ -0,0 +1,49 @@
+package spline
+
+import spline.frontend.*
+import spline.interpreter.*
+
+class InterpreterTest extends munit.FunSuite:
+  private def parseExpr(input: String): Expr = Expr(input)
+  private def parseCond(input: String): Cond = Cond(input)
+  private def parseStat(input: String): Stat = Stat(input)
+
+  test("expr: division by zero") {
+    val expected = Set[Value]()
+    val actual = Interpreter.eval(parseExpr("1 + 2 / 0"), State())
+    assertEquals(actual, expected)
+  }
+
+  test("expr: interval input with division by zero") {
+    val expected = Set[Value]()
+    val actual = Interpreter.eval(parseExpr("[-1, 1] / 0"), State())
+    assertEquals(actual, expected)
+  }
+
+  test("expr: division with interval input") {
+    val expected = Set[Value](-1, 1)
+    val actual = Interpreter.eval(parseExpr("1 / [-1, 1]"), State())
+    assertEquals(actual, expected)
+  }
+
+  test("cond: division by zero") {
+    val expected = Set[State]()
+    val actual = Interpreter.eval(parseCond("1 == 1 / 0"), Set[State]())
+    assertEquals(actual, expected)
+  }
+
+  test("cond: logical or with division by zero") {
+    val defaultSts = Set(State("x" -> 42))
+    val expected = defaultSts
+    val actual = Interpreter.eval(parseCond("1 == 1 / 0 \\/ true"), defaultSts)
+    assertEquals(actual, expected)
+  }
+
+  test("stat: while loop") {
+    val actual = Interpreter.eval(
+      parseStat("Q <- 0; R <- A; while R >= B do R <- R - B; Q <- Q + 1 done"),
+      Set(State("A" -> 10, "B" -> 3)),
+    )
+    val expected = Set(State("A" -> 10, "B" -> 3, "Q" -> 3, "R" -> 1))
+    assertEquals(actual, expected)
+  }
