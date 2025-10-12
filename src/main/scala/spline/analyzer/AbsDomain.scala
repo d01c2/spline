1ef7b06f0c3c363ddbf92abf210399a9e2600952
d01c2 <rickykhj@korea.ac.kr>
2025-10-12 17:28:04 +0900

    feat: work in progress (incomplete)

--- src/main/scala/spline/analyzer/AbsDomain.scala
+++ src/main/scala/spline/analyzer/AbsDomain.scala
@@ -1,28 +1,103 @@
 package spline.analyzer
 
 import spline.frontend.*
+import spline.utils.*
 
-trait Lattice[Elem] {
-  extension (x: Elem) {
+trait AbsDomain[Elem] {
+  type AbsValue = Elem
+  type AbsState = Map[String, AbsValue]
+  extension (st: AbsState) {
+    def apply(v: String): AbsValue = st.getOrElse(v, top)
+  }
+
+  // bottom and top elements
+  def bot: AbsValue
+  def top: AbsValue
+
+  extension (e: Elem) {
+    def isBottom: Boolean
+    def isTop: Boolean
+  }
+
+  // concretization
+  def gamma(x: AbsValue): Set[Value]
+  def gamma(st: AbsState): Map[String, Set[Value]] =
+    if st.values.exists(_.isBottom) then Map.empty
+    else st.view.mapValues(gamma).toMap
+
+  // abstraction
+  def alpha(x: Set[Value]): AbsValue
+  def alpha(st: Map[String, Set[Value]]): AbsState =
+    st.view.mapValues(alpha).toMap
+
+  extension (x: AbsValue) {
     // partial order
-    def ⊑(y: Elem): Boolean
+    def ⊑(y: AbsValue): Boolean
+    def ⊒(y: AbsValue): Boolean = y ⊑ x
+    def ⊐(y: AbsValue): Boolean = !(x ⊑ y)
+    def ⊏(y: AbsValue): Boolean = !(x ⊒ y)
 
     // join and meet operations
-    def ⊔(y: Elem): Elem
-    def ⊓(y: Elem): Elem
-  }
-}
+    def ⊔(y: AbsValue): AbsValue
+    def ⊓(y: AbsValue): AbsValue
 
-trait AbsDomain[Elem] extends Lattice[Elem] {
-  // bottom and top elements
-  def bot: Elem
-  def top: Elem
-
-  // monotonic concretization
-  def gamma(x: Elem): Set[Value]
-
-  extension (x: Elem) {
     // widening operation
-    def ▽(y: Elem): Elem
+    def ▽(y: AbsValue): AbsValue
   }
+
+  extension (x: AbsState) {
+    // partial order
+    def ⊑(y: AbsState): Boolean =
+      (x.keySet ++ y.keySet).forall(k => x(k) ⊑ y(k))
+    def ⊒(y: AbsState): Boolean = y ⊑ x
+    def ⊐(y: AbsState): Boolean = !(x ⊑ y)
+    def ⊏(y: AbsState): Boolean = !(x ⊒ y)
+
+    // join and meet operations
+    def ⊔(y: AbsState): AbsState =
+      (x.keySet ++ y.keySet).map(k => k -> (x(k) ⊔ y(k))).toMap
+    def ⊓(y: AbsState): AbsState =
+      (x.keySet ++ y.keySet).map(k => k -> (x(k) ⊓ y(k))).toMap
+
+    // widening operation
+    def ▽(y: AbsState): AbsState =
+      (x.keySet ++ y.keySet).map(k => k -> (x(k) ▽ y(k))).toMap
+  }
+
+  // abstract transfer functions
+  import Expr.*, Cond.*, Stat.*
+
+  def absNeg(v: AbsValue): AbsValue
+  def absBOp(bop: BOp, v1: AbsValue, v2: AbsValue): AbsValue
+
+  def transfer(e: Expr, st: AbsState): AbsValue = e match
+    case EVar(v)           => st(v)
+    case ENum(c)           => alpha(Set(c))
+    case ENeg(e)           => absNeg(transfer(e, st))
+    case EBOp(bop, e1, e2) => absBOp(bop, transfer(e1, st), transfer(e2, st))
+    case EInput(c1, c2)    => alpha((c1 to c2).toSet)
+
+  def transfer(c: Cond, st: AbsState): AbsState = c match
+    case CCmp(cmp, e1, e2) => ??? // TODO: Condition Abstraction
+    case CBool(b)          => if b then st else Map.empty
+    case CNot(c) =>
+      c match
+        case CCmp(cmp, e1, e2) => transfer(CCmp(cmp.negate, e1, e2), st)
+        case CBool(b)          => transfer(CBool(!b), st)
+        case CNot(c)           => transfer(c, st)
+        case CAnd(c1, c2)      => transfer(COr(CNot(c1), CNot(c2)), st)
+        case COr(c1, c2)       => transfer(CAnd(CNot(c1), CNot(c2)), st)
+    case CAnd(c1, c2) => transfer(c1, st) ⊓ transfer(c2, st)
+    case COr(c1, c2)  => transfer(c1, st) ⊔ transfer(c2, st)
+
+  def transfer(s: Stat, st: AbsState): AbsState = s match
+    case SAssign(x, e) => st + (x -> transfer(e, st))
+    case SSeq(s1, s2)  => transfer(s2, transfer(s1, st))
+    case SCond(c, s1, s2) =>
+      transfer(s1, transfer(c, st)) ⊔ transfer(s2, transfer(CNot(c), st))
+    case SLoop(c, s) =>
+      def F(x: AbsState): AbsState = x ▽ (st ⊔ transfer(s, transfer(c, x)))
+      transfer(CNot(c), fixpoint(F)(st))
+    case SNop       => st
+    case SAssert(c) => transfer(c, st)
 }
--- src/main/scala/spline/analyzer/interval/Interval.scala
+++ src/main/scala/spline/analyzer/interval/Interval.scala
@@ -1,9 +1,10 @@
 package spline.analyzer.interval
 
 import spline.frontend.*
+import spline.utils.*
 
-case class Infinity(pos: Boolean) {
-  override def toString: String = if pos then "+∞" else "-∞"
+case class Infinity(sign: Boolean) {
+  override def toString: String = if sign then "+∞" else "-∞"
 }
 
 type Bound = Value | Infinity
@@ -19,9 +20,38 @@ given Ordering[Bound] with {
     case (v1: Value, v2: Value)             => v1 compare v2
 }
 
-extension (a: Bound) {
-  def min(b: Bound)(using ord: Ordering[Bound]) = ord.min(a, b)
-  def max(b: Bound)(using ord: Ordering[Bound]) = ord.max(a, b)
+def min(bs: Bound*): Bound = bs.min
+def min(bs: Iterable[Bound]): Bound = bs.min
+def max(bs: Bound*): Bound = bs.max
+def max(bs: Iterable[Bound]): Bound = bs.max
+
+extension (self: Bound) {
+  def unary_- : Bound = self match
+    case Infinity(sign) => Infinity(!sign)
+    case v: Value       => -v
+  def +(other: Bound): Bound = (self, other) match
+    case (Infinity(true), _) | (_, Infinity(true))   => Infinity(true)
+    case (Infinity(false), _) | (_, Infinity(false)) => Infinity(false)
+    case (v1: Value, v2: Value)                      => v1 + v2
+  def -(other: Bound): Bound = self + (-other)
+  def *(other: Bound): Bound = (self, other) match
+    case (Infinity(sign1), Infinity(sign2)) => Infinity(sign1 == sign2)
+    case (Infinity(sign), v: Value) =>
+      if v == 0 then 0
+      else { if (v > 0) Infinity(sign) else Infinity(!sign) }
+    case (v: Value, Infinity(sign)) =>
+      if v == 0 then 0
+      else { if (v > 0) Infinity(sign) else Infinity(!sign) }
+    case (v1: Value, v2: Value) => v1 * v2
+  def /(other: Bound): Bound = (self, other) match
+    case (Infinity(_), Infinity(_)) => 0
+    case (v: Value, Infinity(sign)) => 0
+    case (_, v: Value) if v == 0 =>
+      error(s"division by zero: $self / $other")
+    case (bound, v: Value) =>
+      bound match
+        case Infinity(sign) => if (v > 0) Infinity(sign) else Infinity(!sign)
+        case v0: BigInt     => v0 / v
 }
 
 case class Interval(lb: Bound, ub: Bound) {
