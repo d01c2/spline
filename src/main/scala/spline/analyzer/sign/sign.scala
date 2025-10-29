40cc2d8ad27c3b8e21eecc8e477a4fb2cedd6949
taxor03 <ryan040@korea.ac.kr>
2025-10-29 14:07:16 +0900

    fix: rm uneccessary part in sign domain

--- src/main/scala/spline/analyzer/sign/sign.scala
+++ src/main/scala/spline/analyzer/sign/sign.scala
@@ -7,24 +7,6 @@ case class Infinity(sign: Boolean) {
   override def toString: String = if sign then "+∞" else "-∞"
 }
 
-type Bound = Value | Infinity
-
-given Ordering[Bound] with {
-  def compare(x: Bound, y: Bound): Int = (x, y) match
-    case (Infinity(true), Infinity(true))   => 0
-    case (Infinity(false), Infinity(false)) => 0
-    case (Infinity(true), _)                => 1
-    case (_, Infinity(true))                => -1
-    case (Infinity(false), _)               => -1
-    case (_, Infinity(false))               => 1
-    case (v1: Value, v2: Value)             => v1 compare v2
-}
-
-def min(bs: Bound*): Bound = bs.min
-def min(bs: Iterable[Bound]): Bound = bs.min
-def max(bs: Bound*): Bound = bs.max
-def max(bs: Iterable[Bound]): Bound = bs.max
-
 enum Sign {
   case Top
   case PosOrZero
